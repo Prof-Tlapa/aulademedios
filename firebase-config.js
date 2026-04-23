eca2f59db6022470b753b97c802b226d70c56925
Prof-Tlapa <jose.tlapa@cobachbc.edu.mx>
2026-04-23 14:40:37 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase-config.js
 const firebaseConfig = {
-  apiKey: "AIzaSyAsP0mFuWyJBnH2w1c_victOiZJo7hu-Ng",
-  authDomain: "aulademedios-b5bb6.firebaseapp.com",
-  projectId: "aulademedios-b5bb6",
-  storageBucket: "aulademedios-b5bb6.firebasestorage.app",
-  messagingSenderId: "993147947086",
-  appId: "1:993147947086:web:d6a0a555db70791f501b1f",
-  measurementId: "G-MDBTHJGKTN"
+    apiKey: "AIzaSyB3o9vWk1xJQ5L8mNp2R7tUyV3wX4zA6bC8dE9fG",
+    authDomain: "aulademedios.firebaseapp.com",
+    projectId: "aulademedios",
+    storageBucket: "aulademedios.firebasestorage.app",
+    messagingSenderId: "123456789012",
+    appId: "1:123456789012:web:abcdef1234567890"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+console.log("✅ Firebase conectado correctamente");
+console.log("📁 Base de datos lista:", db);
